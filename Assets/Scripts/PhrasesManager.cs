9db1f49d8530e433f48c8f4fbbcc4769f8d2e6f6
salmankamal <salman.kamal9300@gmail.com>
2025-12-25 04:01:01 +0500

    phrases manager

--- Assets/Scripts/PhrasesManager.cs
+++ Assets/Scripts/PhrasesManager.cs
@@ -1,28 +1,47 @@
+using TMPro;
+using Unity.Properties;
 using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
 using System.Collections.Generic;
-using System;
+using UnityEngine.Rendering;
 
-public enum Phrases1Connectors
+[System.Serializable]
+public struct ContentPictureTrio
 {
-    eating,
-    sleeping,
-    drinking,
-    sitting,
-    washing,
-    brushing,
-    ball,
-    where
+    public string content;
+    public Sprite phraseImage;
+    public Sprite sightWordImage;
+}
+
+[System.Serializable]
+public struct PBI
+{
+    public string phraseContext;
+    public int index;
+    public List<ContentPictureTrio> contentList;
+}
+
+
+[System.Serializable]
+public struct PhrasesBookInformation
+{
+    public Books book;
+    public bool enabled;
+    public List<PBI> PBIList;
+
 }
 
 public class PhrasesManager : MonoBehaviour
 {
     public static PhrasesManager Instance { get; private set; }
-    public static int bookSelected;
-    public static Phrases1Connectors connectorSelected;
-    private List<ContentPicturePair> content = new List<ContentPicturePair>();
+    [SerializeField] private List<PhrasesBookInformation> booksList = new List<PhrasesBookInformation>();
+
+    private List<ContentPictureTrio> content = new List<ContentPictureTrio>();
 
     void Awake()
     {
+        // Ensure only one instance exists
         if (Instance != null && Instance != this)
         {
             Destroy(gameObject); // Destroy duplicate instances
@@ -30,20 +49,85 @@ public class PhrasesManager : MonoBehaviour
         Instance = this;
     }
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
 
     }
 
-    // Update is called once per frame
     void Update()
     {
 
     }
 
-    public void SetBook()
+    public List<ContentPictureTrio> GetCurrentEnabledDictionary()
     {
+        foreach (PhrasesBookInformation book in booksList)
+        {
+            if (book.enabled)
+            {
+                foreach (PBI b in book.PBIList)
+                {
+                    content.AddRange(b.contentList);
+                }
+            }
+        }
 
+        return content;
+    }
+
+    public List<ContentPictureTrio> GetRequestedBook(Books requestedBook, string requestedContext)
+    {
+        List<ContentPictureTrio> displayBook = new List<ContentPictureTrio>();
+        int check = 0;
+
+        foreach (PhrasesBookInformation book in booksList)
+        {
+            if (book.book == requestedBook)
+            {
+                foreach (PBI b in book.PBIList)
+                {
+                    if (b.phraseContext == requestedContext)
+                    {
+                        displayBook.AddRange(b.contentList);
+                        check = 1;
+                    }
+                }
+                if (check == 1)
+                {
+                    return displayBook;
+                }
+            }
+        }
+
+        return null;
+    }
+
+    public void SetBookEnabled(Books requestedBook, bool enabled)
+    {
+        for (int i = 0; i < booksList.Count; i++)
+        {
+            if (booksList[i].book == requestedBook)
+            {
+                PhrasesBookInformation temp = booksList[i];
+                temp.enabled = enabled;
+                booksList[i] = temp;
+                break;
+            }
+        }
+    }
+
+    public List<string> ExtractSightWords(List<PBI> list)
+    {
+        List<string> sightWords = new List<string>();
+
+        foreach (PBI b in list)
+        {
+            foreach (ContentPictureTrio c in b.contentList)
+            {
+                sightWords.Add(c.content.Split()[b.index]);
+            }
+        }
+
+        return sightWords;
     }
 }
