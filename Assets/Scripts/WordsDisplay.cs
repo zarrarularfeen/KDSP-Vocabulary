716ccc5ace2bb0fc79b997b7ac35d94d3cc4221b
salmankamal <salman.kamal9300@gmail.com>
2026-01-07 00:32:40 +0500

    wordsdisplay multi selectable buttons logic added

--- Assets/Scripts/WordsDisplay.cs
+++ Assets/Scripts/WordsDisplay.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using UnityEngine.Rendering;
 using NUnit.Framework;
 using Unity.VisualScripting;
+using UnityEngine.InputSystem.LowLevel;
 
 public enum WordsDisplayMode
 {
@@ -21,9 +22,9 @@ public enum GameMode
     Phrases,
     Sentences
 }
+
 public class WordsDisplay : MonoBehaviour
 {
-
     private List<ContentPictureAudioTrio> content = new List<ContentPictureAudioTrio>();
     private List<ContextListEntry> contextList = new List<ContextListEntry>();
     private TextMeshProUGUI wordText;
@@ -34,6 +35,10 @@ public class WordsDisplay : MonoBehaviour
     public static WordsDisplayMode currentMode;
     public static GameMode currentGameMode;
     public static WordsDisplay Instance { get; private set; }
+
+    private Dictionary<string, Button> entryDict = new Dictionary<string, Button>();
+    [SerializeField] private Sprite deselectedImage;
+    [SerializeField] private Sprite selectedImage;
 
 
     void Awake()
@@ -86,25 +91,37 @@ public class WordsDisplay : MonoBehaviour
     void DisplayWords()
     {
         if (currentMode == WordsDisplayMode.SightWords || currentMode == WordsDisplayMode.Vocabulary)
+        {
             foreach (ContentPictureAudioTrio pair in content)
             {
-                Button newButton = Instantiate(wordButton, wordsGrid.transform);
-                newButton.gameObject.SetActive(true);
-                wordText = newButton.GetComponentInChildren<TextMeshProUGUI>();
-                wordText.text = pair.content;
-                newButton.onClick.AddListener(() => OnWordButtonClicked(pair));
-                Debug.Log("button added to grid: " + pair.content);
+                if (!entryDict.ContainsKey(pair.content))
+                {
+                    Button newButton = Instantiate(wordButton, wordsGrid.transform);
+                    newButton.gameObject.SetActive(true);
+                    wordText = newButton.GetComponentInChildren<TextMeshProUGUI>();
+                    wordText.text = pair.content;
+                    newButton.onClick.AddListener(() => OnWordButtonClicked(pair));
+                    Debug.Log("button added to grid: " + pair.content);
+
+                    entryDict.Add(pair.content, newButton);
+                }
             }
+        }
         else if (currentMode == WordsDisplayMode.Phrases)
         {
             foreach (ContextListEntry entry in contextList)
             {
-                Button newButton = Instantiate(wordButton, wordsGrid.transform);
-                newButton.gameObject.SetActive(true);
-                wordText = newButton.GetComponentInChildren<TextMeshProUGUI>();
-                wordText.text = entry.context;
-                newButton.onClick.AddListener(() => OnWordButtonClickedPhrases(entry));
-                Debug.Log("button added to grid phrases: " + entry.context);
+                if (!entryDict.ContainsKey(entry.context))
+                {
+                    Button newButton = Instantiate(wordButton, wordsGrid.transform);
+                    newButton.gameObject.SetActive(true);
+                    wordText = newButton.GetComponentInChildren<TextMeshProUGUI>();
+                    wordText.text = entry.context;
+                    newButton.onClick.AddListener(() => OnWordButtonClickedPhrases(entry));
+                    Debug.Log("button added to grid phrases: " + entry.context);
+
+                    entryDict.Add(entry.context, newButton);
+                }
             }
         }
     }
@@ -113,17 +130,20 @@ public class WordsDisplay : MonoBehaviour
     {
         Debug.Log("Word button clicked: " + pair.content);
         // Add your logic here for what happens when a word button is clicked
-        
+
         if (currentGameMode == GameMode.Vocabulary)
         {
             if (!VocabularyMatching.selectedContent.Contains(pair))
             {
                 VocabularyMatching.selectedContent.Add(pair);
+                SpriteSwap(pair.content, true);
                 Debug.Log("Added to selectedContent: " + pair.content);
             }
             else
             {
-                Debug.Log("Already in selectedContent: " + pair.content);
+                VocabularyMatching.selectedContent.Remove(pair);
+                SpriteSwap(pair.content, false);
+                Debug.Log("Remove from selectedContent: " + pair.content);
             }
         }
         else if (currentGameMode == GameMode.Phrases)
@@ -131,11 +151,14 @@ public class WordsDisplay : MonoBehaviour
             if (!PhrasesLevelManager.selectedContent.Contains(pair))
             {
                 PhrasesLevelManager.selectedContent.Add(pair);
+                SpriteSwap(pair.content, true);
                 Debug.Log("Added to selectedContent: " + pair.content);
             }
             else
             {
-                Debug.Log("Already in selectedContent: " + pair.content);
+                PhrasesLevelManager.selectedContent.Remove(pair);
+                SpriteSwap(pair.content, false);
+                Debug.Log("Removed from selectedContent: " + pair.content);
             }
         }
     }
@@ -147,28 +170,61 @@ public class WordsDisplay : MonoBehaviour
         if (!PhrasesLevelManager.selectedContextList.Contains(entry))
         {
             PhrasesLevelManager.selectedContextList.Add(entry);
+            SpriteSwap(entry.context, true);
             Debug.Log("Added to selectedContextList: " + entry.context);
         }
         else
         {
-            Debug.Log("Already in selectedContextList: " + entry.context);
+            PhrasesLevelManager.selectedContextList.Remove(entry);
+            SpriteSwap(entry.context, false);
+            Debug.Log("Removed from selectedContextList: " + entry.context);
         }
     }
 
     void OnNextButtonClicked(Button nextButton)
     {
         if (currentGameMode == GameMode.Vocabulary)
+        {
+            VocabularyMatching.selectedContent.Clear();
+            PhrasesLevelManager.selectedContextList.Clear();
             nextButton.onClick.AddListener(() => SceneController.Instance.OpenLevelSelect("VocabularyMatching"));
+        }
         else if (currentGameMode == GameMode.Phrases)
+        {
+            VocabularyMatching.selectedContent.Clear();
+            PhrasesLevelManager.selectedContextList.Clear();
             nextButton.onClick.AddListener(() => SceneController.Instance.OpenLevelSelect("PhrasesLevel"));
+        }
     }
+
 
     void OnBackButtonClicked(Button backButton)
     {
         if (currentGameMode == GameMode.Vocabulary)
+        {
+            VocabularyMatching.selectedContent.Clear();
+            PhrasesLevelManager.selectedContextList.Clear();
             backButton.onClick.AddListener(() => SceneController.Instance.OpenLevelSelect("EnableBooksVocabulary"));
+        }
         else if (currentGameMode == GameMode.Phrases)
+        {
+            VocabularyMatching.selectedContent.Clear();
+            PhrasesLevelManager.selectedContextList.Clear();
             backButton.onClick.AddListener(() => SceneController.Instance.OpenLevelSelect("EnableBooksPhrases"));
+        }
+
+    }
+
+    public void SpriteSwap(string name, bool enabled)
+    {
+        if (enabled)
+        {
+            entryDict[name].gameObject.GetComponent<Image>().sprite = selectedImage;
+        }
+        else
+        {
+            entryDict[name].gameObject.GetComponent<Image>().sprite = deselectedImage;
+        }
     }
 
 }
